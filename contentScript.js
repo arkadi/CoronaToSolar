cdf35526b500265ca5875957fbea1fa1d5b5cfd0
arkadi zurabiani <azkaban0503@gmail.com>
2020-05-11 03:20:14 +0400

    master

--- contentScript.js
+++ contentScript.js
@@ -2,15 +2,11 @@ $(document).ready(function(){
     var sites=$("a");
    for(var i=0;i<sites.length;i++){
     if($(sites[i]).attr("href") !=undefined && $(sites[i]).attr("href").indexOf("https://forums.coronalabs.com/")==0){
-        var title="";
-        if($(sites[i]).find("h3").length!=0){
-            title=escape($(sites[i]).find("h3")[0].innerText.replace(" - Corona Labs Forums",""));
-            console.log(title);
-        }else{
-            title=escape($(sites[i]).text().replace("...",""));
-            console.log(title);
-        }
-        $(sites[i]).parent().append("<div><a href='https://forums.solar2d.com/search?q="+title+"'>Redirect to Solar2d forum</a></div>")
+     
+       var  title=($(sites[i]).find("h3").length!=0)?($(sites[i]).find("h3")[0].innerText):($(sites[i]).text().replace("...",""));
+        title=title.replace(" - Corona Labs Forums","")
+        
+        $(sites[i]).parent().append("<div><a href='https://forums.solar2d.com/search?q="+escape(title)+"'>Redirect to Solar2d forum</a></div>")
     }
    }
    
